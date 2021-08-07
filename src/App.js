2d30840de34c20ff2022fdd26fc791ead81c72b7
jason <2B3>
2021-08-06 20:10:27 -0400

    commit 5: Let App.js access context content

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
+import React from "react";
 import "./App.css";
-
 import Counter from "./components/Counter";
 import CounterContextProvider from "./contexts/CounterContext";
+import { useCounter } from "./contexts/CounterContext";
 
 function App(props) {
-
+  const { count, increaseCount, decreaseCount } = useCounter(); //! try to import our custom hook
   return (
-    <CounterContextProvider>
+    <React.Fragment>
       <h1>Context API</h1>
-      <Counter/>
-    </CounterContextProvider>
+      <Counter />
+    </React.Fragment>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import CounterContextProvider from './contexts/CounterContext'
 ReactDOM.render(
   <React.StrictMode>
+  <CounterContextProvider>
     <App />
+  </CounterContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
